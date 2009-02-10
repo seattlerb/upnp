060080ada3159ef0f945cc670176565e68e08fd8
Eric Hodel <drbrain@segment7.net>
2009-02-10 13:41:01 -0800

    Workaround for missing constants on windows.
    [git-p4: depot-paths = "//src/UPnP/dev/": change = 4578]

--- lib/UPnP/SSDP.rb
+++ lib/UPnP/SSDP.rb
@@ -756,3 +756,24 @@ ST: #{search_target}\r
 
 end
 
+# :stopdoc:
+
+##
+# Workaround for mising constants on Windows
+
+module Socket::Constants
+  IP_ADD_MEMBERSHIP = 12 unless defined? IP_ADD_MEMBERSHIP
+  IP_MULTICAST_LOOP = 11 unless defined? IP_MULTICAST_LOOP
+  IP_MULTICAST_TTL  = 10 unless defined? IP_MULTICAST_TTL
+  IP_TTL            =  4 unless defined? IP_TTL
+end
+
+class Socket
+  IP_ADD_MEMBERSHIP = 12 unless defined? IP_ADD_MEMBERSHIP
+  IP_MULTICAST_LOOP = 11 unless defined? IP_MULTICAST_LOOP
+  IP_MULTICAST_TTL  = 10 unless defined? IP_MULTICAST_TTL
+  IP_TTL            =  4 unless defined? IP_TTL
+end
+
+# :startdoc:
+
